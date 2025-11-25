15c3ff1de37b9c42614ac1a7f0d4b20fa58c4b01
Guilherme Celente <guilherme.celente@outlook.com>
2025-11-25 14:52:07 -0300

    feat/2.1: implement lifecycle and versioning configuration to s3

--- infra/s3.tf
+++ infra/s3.tf
@@ -7,3 +7,30 @@ resource "aws_s3_bucket" "gateway-s3-files" {
     Environment  = "Dev"
   }
 }
+
+# Enables versioning for the S3 bucket
+resource "aws_s3_bucket_versioning" "gateway_s3_files_versioning" {
+  bucket = aws_s3_bucket.gateway-s3-files.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+##Configures lifecycle rule to expire non-current object versions after 10 days
+resource "aws_s3_bucket_lifecycle_configuration" "gateway_s3_files_lifecycle" {
+  bucket = aws_s3_bucket.gateway-s3-files.id
+
+  rule {
+    id     = "Expire old versions"
+    status = "Enabled"
+
+    noncurrent_version_expiration {
+      noncurrent_days = 10
+    }
+  }
+
+  depends_on = [
+    aws_s3_bucket_versioning.gateway_s3_files_versioning
+  ]
+}
